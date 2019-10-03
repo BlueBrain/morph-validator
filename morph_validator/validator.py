5f4495557fb23c7ce5bcf463878cea38ce96a408
Aleksei Sanin <aleksei.sanin@epfl.ch>
2019-10-03 17:06:08 +0200

    draft of valid morphologies collection
    Change-Id: I68ae18433c27417b7510235b1148b54325d6de54

---
+++ morph_validator/validator.py
@@ -0,0 +1,114 @@
+"""
+Example code to show complete use case.
+"""
+import itertools
+import logging
+from enum import Enum
+from pathlib import Path
+
+import neurom as nm
+import numpy as np
+import pandas as pd
+from lxml import etree
+from neurom import NeuriteType
+
+L = logging.getLogger(__name__)
+pd.options.display.width = 0
+
+
+class DISCRETE_FEATURE_NAMES(Enum):
+    LEN = 'total_length'
+    SURFACE_AREA = 'total_area_per_neurite'
+    VOLUMES = 'neurite_volumes'
+    NUMBER_OF_SECTIONS = 'number_of_sections'
+    NUMBER_OF_BIFURCATIONS = 'number_of_bifurcations'
+    NUMBER_OF_TERMINATIONS = 'number_of_terminations'
+
+
+class CONTINUOUS_FEATURE_NAMES(Enum):
+    SECTION_LEN = 'section_lengths'
+    SECTION_RADIAL_DISTANCES = 'section_radial_distances'
+
+
+NEURITES = (NeuriteType.soma,
+            NeuriteType.axon,
+            NeuriteType.basal_dendrite,
+            NeuriteType.apical_dendrite,
+            NeuriteType.undefined,)
+NEURITE_NAMES = [type.name for type in NEURITES]
+
+CUSTOM_FEATURE_LOAD = {
+    DISCRETE_FEATURE_NAMES.SURFACE_AREA.value: {
+        NeuriteType.soma.name: lambda neuron: nm.get('soma_surface_areas', neuron),
+    }
+}
+
+
+def get_discrete_features(neuron) -> pd.DataFrame:
+    feature_names = [name.value for name in DISCRETE_FEATURE_NAMES]
+    df = get_features(neuron, feature_names)
+    df = df.applymap(np.sum)
+    df.loc[NeuriteType.all.name] = df.sum()
+    return df
+
+
+def get_continuous_features(neuron) -> pd.DataFrame:
+    feature_names = [name.value for name in CONTINUOUS_FEATURE_NAMES]
+    df = get_features(neuron, feature_names)
+    df.loc[NeuriteType.all.name] = df.aggregate(lambda x: np.concatenate(x).tolist())
+    return df
+
+
+def get_features(neuron, feature_names) -> pd.DataFrame:
+    df = pd.DataFrame(index=NEURITE_NAMES, columns=feature_names)
+    for neurite, feature_name in itertools.product(NEURITES, feature_names):
+        val = None
+        if feature_name in CUSTOM_FEATURE_LOAD:
+            if neurite.name in CUSTOM_FEATURE_LOAD[feature_name]:
+                val = CUSTOM_FEATURE_LOAD[feature_name][neurite.name](neuron)
+        if val is None:
+            val = nm.get(feature_name, neuron, neurite_type=neurite)
+        df.loc[neurite.name, feature_name] = val
+    return df
+
+
+def get_mtype_dict(db_file: Path) -> dict:
+    root = etree.parse(str(db_file)).getroot()
+    mtype_dict = {}
+    for morphology in root.iterfind('.//morphology'):
+        name = morphology.findtext('name')
+        if not name:
+            L.warning('Empty morphology name in %s', db_file)
+        mtype = morphology.findtext('mtype')
+        if not mtype:
+            L.warning('Empty morphology mtype in %s', db_file)
+        if name in mtype_dict and mtype_dict[name] != mtype:
+            L.warning('Multiple mtypes %s %s for %s', mtype, mtype_dict[name], name)
+        mtype_dict[name] = mtype
+    return mtype_dict
+
+
+def build_valid_morphologies(morph_dirpath: Path):
+    if not morph_dirpath.is_dir():
+        raise ValueError(
+            '"{}" must be a directory with morphology files'.format(morph_dirpath))
+    mtype_dict = get_mtype_dict(morph_dirpath.joinpath('neuronDB.xml'))
+    discrete_features = []
+    continuous_features = []
+    features_index = []
+    features_index_names = ['filename', 'mtype', 'neurite']
+    for file in morph_dirpath.iterdir():
+        if file.suffix in ['.h5', '.swc', '.asc']:
+            neuron = nm.load_neuron(str(file))
+            mtype = mtype_dict[neuron.name]
+            features_index.append((neuron.name, mtype))
+            discrete_features.append(get_discrete_features(neuron))
+            continuous_features.append(get_continuous_features(neuron))
+    discrete_features = pd.concat(
+        discrete_features, keys=features_index, names=features_index_names)
+    continuous_features = pd.concat(
+        continuous_features, keys=features_index, names=features_index_names)
+
+
+if __name__ == '__main__':
+    build_valid_morphologies(Path('../tests/data/valid_morphologies'))
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import imp
+import sys
+
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    sys.exit("Sorry, Python < 3.6 is not supported")
+
+VERSION = imp.load_source("", "morph_validator/version.py").__version__
+
+setup(
+    name="morph-validator",
+    author="BlueBrain NSE",
+    author_email="bbp-ou-nse@groupes.epfl.ch",
+    version=VERSION,
+    description="tool for validating morphologies against existing morphologies of the same type",
+    url="https://bbpteam.epfl.ch/documentation/projects/morph-validator",
+    project_urls={
+        "Tracker": "https://bbpteam.epfl.ch/project/issues/projects/NSETM/issues",
+        "Source": "ssh://bbpcode.epfl.ch/nse/morph-validator",
+    },
+    license="BBP-internal-confidential",
+    install_requires=[
+        'pandas',
+        'neurom',
+        'lxml',
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
